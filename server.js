9d551f4fb5abf39fb56a339ef5e3ca287c6dcda4
Michał Puchała <michalpuchala86@gmail.com>
2025-11-09 10:23:37 +0100

    add snyk and helmet

--- server.js
+++ server.js
@@ -5,9 +5,11 @@ const path = require('path');
 const socket = require('socket.io');
 const http = require('http');
 const mongoose = require('mongoose');
+const helmet = require('helmet');
 
 const app = express();
 const server = http.createServer(app);
+app.use(helmet());
 app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '/client/build')));
